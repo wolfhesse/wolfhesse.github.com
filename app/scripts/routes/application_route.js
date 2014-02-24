b79901f19c910b916396f9f5f29645d1b6c5eb75
rogera <wolfgang.schuessel@gmail.com>
2014-02-24 19:47:47 +0100

    wp

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -2,6 +2,6 @@ NdEmberDemo3D.ApplicationRoute = Ember.Route.extend({
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
     model: function () {
-        return ['red', 'yellow', 'black', 'blue', 'orange', 'about', 'r2kd805'];
+        return ['about', 'r2kd805'];
     }
 });
