3f72632e617aa1cb4950c3a5357fa0f1c454ca3f
rogera <wolfgang.schuessel@gmail.com>
2014-02-24 19:00:01 +0100

    wp

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,7 +1,7 @@
 NdEmberDemo3D.Router.reopen({
     enableLogging: true,
     location: 'history',
-    rootURL: '/demo3/'
+    rootURL: '/demo3'
 });
 NdEmberDemo3D.Router.map(function () {
     this.resource('about');
