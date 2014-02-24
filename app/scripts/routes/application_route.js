2e08dfabfd24f6ffde3c699e358f5159d09cd88d
rogera <wolfgang.schuessel@gmail.com>
2014-02-24 19:46:55 +0100

    wp

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -2,11 +2,6 @@ NdEmberDemo3D.ApplicationRoute = Ember.Route.extend({
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
     model: function () {
-        return ['red', 'yellow', 'black', 'blue', 'orange'];
+        return ['red', 'yellow', 'black', 'blue', 'orange', 'about', 'r2kd805'];
     }
-//    ,
-//    rootUrl: '/demo3/',
-//    redirect: function () {
-//        this.transitionTo('/demo3/');
-//    }
 });
