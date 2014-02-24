98c2280846b26f73ded8072ce7e3d5e3c9d36086
rogera <wolfgang.schuessel@gmail.com>
2014-02-24 19:53:36 +0100

    wp

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,6 +1,6 @@
 NdEmberDemo3D.Router.reopen({
     enableLogging: true,
-    location: 'history', rootURL: '/demo3/'
+    location: 'history', rootURL: '/demo3'
 });
 NdEmberDemo3D.Router.map(function () {
     this.resource('about');
