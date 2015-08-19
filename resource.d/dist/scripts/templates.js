92751801ac64c40f49b9be931c6bea40e1aa218f
wolfhesse <wolfgang.schuessel@gmail.com>
2015-08-19 17:08:29 +0200

    wp

---
+++ resource.d/dist/scripts/templates.js
@@ -0,0 +1,128 @@
+Ember.TEMPLATES["about"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options, self=this, helperMissing=helpers.helperMissing;
+
+function program1(depth0,data) {
+  
+  
+  data.buffer.push("r2kd805");
+  }
+
+  data.buffer.push("<div class='well about'>\n    <div class=\"largewidth\">\n        ");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || depth0['link-to']),stack1 ? stack1.call(depth0, "r2kd805", options) : helperMissing.call(depth0, "link-to", "r2kd805", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n    </div>\n    <div>\n        <p>Yehuda Katz is a member of the <a href=\"http://emberjs.com\">Ember.js</a>, <a href=\"http://rubyonrails.org\">Ruby\n            on Rails</a>\n            and <a href=\"http://www.jquery.com\">jQuery</a> Core Teams; he spends his daytime hours at the startup he\n            founded,\n            <a href=\"http://www.tilde.io\">Tilde Inc.</a>.</p>\n\n        <p>Yehuda is co-author of best-selling <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_176\">jQuery\n            in\n            Action</a> and\n            <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_145\">Rails 3 in Action</a>.</p>\n\n        <p>He spends most of his time hacking on open source?his main projects, along with others, like <a\n                href=\"https://github.com/wycats/thor\">Thor</a>,\n            <a href=\"http://www.handlebarsjs.com\">Handlebars</a> and <a\n                    href=\"https://github.com/carlhuda/janus\">Janus</a>?or\n            traveling the world doing evangelism work.</p>\n\n        <p>He can be found on Twitter as <a href=\"http://www.twitter.com/wycats\">@wycats</a>.</p>\n    </div>\n    <div>\n        <p>Yehuda Katz is a member of the <a href=\"http://emberjs.com\">Ember.js</a>, <a href=\"http://rubyonrails.org\">Ruby\n            on Rails</a>\n            and <a href=\"http://www.jquery.com\">jQuery</a> Core Teams; he spends his daytime hours at the startup he\n            founded,\n            <a href=\"http://www.tilde.io\">Tilde Inc.</a>.</p>\n\n        <p>Yehuda is co-author of best-selling <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_176\">jQuery\n            in\n            Action</a> and\n            <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_145\">Rails 3 in Action</a>.</p>\n\n        <p>He spends most of his time hacking on open source?his main projects, along with others, like <a\n                href=\"https://github.com/wycats/thor\">Thor</a>,\n            <a href=\"http://www.handlebarsjs.com\">Handlebars</a> and <a\n                    href=\"https://github.com/carlhuda/janus\">Janus</a>?or\n            traveling the world doing evangelism work.</p>\n\n        <p>He can be found on Twitter as <a href=\"http://www.twitter.com/wycats\">@wycats</a>.</p>\n    </div>\n    <div>\n        <p>Yehuda Katz is a member of the <a href=\"http://emberjs.com\">Ember.js</a>, <a href=\"http://rubyonrails.org\">Ruby\n            on Rails</a>\n            and <a href=\"http://www.jquery.com\">jQuery</a> Core Teams; he spends his daytime hours at the startup he\n            founded,\n            <a href=\"http://www.tilde.io\">Tilde Inc.</a>.</p>\n\n        <p>Yehuda is co-author of best-selling <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_176\">jQuery\n            in\n            Action</a> and\n            <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_145\">Rails 3 in Action</a>.</p>\n\n        <p>He spends most of his time hacking on open source?his main projects, along with others, like <a\n                href=\"https://github.com/wycats/thor\">Thor</a>,\n            <a href=\"http://www.handlebarsjs.com\">Handlebars</a> and <a\n                    href=\"https://github.com/carlhuda/janus\">Janus</a>?or\n            traveling the world doing evangelism work.</p>\n\n        <p>He can be found on Twitter as <a href=\"http://www.twitter.com/wycats\">@wycats</a>.</p>\n    </div>\n    <div>\n        <p>Yehuda Katz is a member of the <a href=\"http://emberjs.com\">Ember.js</a>, <a href=\"http://rubyonrails.org\">Ruby\n            on Rails</a>\n            and <a href=\"http://www.jquery.com\">jQuery</a> Core Teams; he spends his daytime hours at the startup he\n            founded,\n            <a href=\"http://www.tilde.io\">Tilde Inc.</a>.</p>\n\n        <p>Yehuda is co-author of best-selling <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_176\">jQuery\n            in\n            Action</a> and\n            <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_145\">Rails 3 in Action</a>.</p>\n\n        <p>He spends most of his time hacking on open source?his main projects, along with others, like <a\n                href=\"https://github.com/wycats/thor\">Thor</a>,\n            <a href=\"http://www.handlebarsjs.com\">Handlebars</a> and <a\n                    href=\"https://github.com/carlhuda/janus\">Janus</a>?or\n            traveling the world doing evangelism work.</p>\n\n        <p>He can be found on Twitter as <a href=\"http://www.twitter.com/wycats\">@wycats</a>.</p>\n    </div>\n    <div>\n        <p>Yehuda Katz is a member of the <a href=\"http://emberjs.com\">Ember.js</a>, <a href=\"http://rubyonrails.org\">Ruby\n            on Rails</a>\n            and <a href=\"http://www.jquery.com\">jQuery</a> Core Teams; he spends his daytime hours at the startup he\n            founded,\n            <a href=\"http://www.tilde.io\">Tilde Inc.</a>.</p>\n\n        <p>Yehuda is co-author of best-selling <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_176\">jQuery\n            in\n            Action</a> and\n            <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_145\">Rails 3 in Action</a>.</p>\n\n        <p>He spends most of his time hacking on open source?his main projects, along with others, like <a\n                href=\"https://github.com/wycats/thor\">Thor</a>,\n            <a href=\"http://www.handlebarsjs.com\">Handlebars</a> and <a\n                    href=\"https://github.com/carlhuda/janus\">Janus</a>?or\n            traveling the world doing evangelism work.</p>\n\n        <p>He can be found on Twitter as <a href=\"http://www.twitter.com/wycats\">@wycats</a>.</p>\n    </div>\n    <div>\n        <p>Yehuda Katz is a member of the <a href=\"http://emberjs.com\">Ember.js</a>, <a href=\"http://rubyonrails.org\">Ruby\n            on Rails</a>\n            and <a href=\"http://www.jquery.com\">jQuery</a> Core Teams; he spends his daytime hours at the startup he\n            founded,\n            <a href=\"http://www.tilde.io\">Tilde Inc.</a>.</p>\n\n        <p>Yehuda is co-author of best-selling <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_176\">jQuery\n            in\n            Action</a> and\n            <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_145\">Rails 3 in Action</a>.</p>\n\n        <p>He spends most of his time hacking on open source?his main projects, along with others, like <a\n                href=\"https://github.com/wycats/thor\">Thor</a>,\n            <a href=\"http://www.handlebarsjs.com\">Handlebars</a> and <a\n                    href=\"https://github.com/carlhuda/janus\">Janus</a>?or\n            traveling the world doing evangelism work.</p>\n\n        <p>He can be found on Twitter as <a href=\"http://www.twitter.com/wycats\">@wycats</a>.</p>\n    </div>\n    <div>\n        <p>Yehuda Katz is a member of the <a href=\"http://emberjs.com\">Ember.js</a>, <a href=\"http://rubyonrails.org\">Ruby\n            on Rails</a>\n            and <a href=\"http://www.jquery.com\">jQuery</a> Core Teams; he spends his daytime hours at the startup he\n            founded,\n            <a href=\"http://www.tilde.io\">Tilde Inc.</a>.</p>\n\n        <p>Yehuda is co-author of best-selling <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_176\">jQuery\n            in\n            Action</a> and\n            <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_145\">Rails 3 in Action</a>.</p>\n\n        <p>He spends most of his time hacking on open source?his main projects, along with others, like <a\n                href=\"https://github.com/wycats/thor\">Thor</a>,\n            <a href=\"http://www.handlebarsjs.com\">Handlebars</a> and <a\n                    href=\"https://github.com/carlhuda/janus\">Janus</a>?or\n            traveling the world doing evangelism work.</p>\n\n        <p>He can be found on Twitter as <a href=\"http://www.twitter.com/wycats\">@wycats</a>.</p>\n    </div>\n    <div>\n        <p>Yehuda Katz is a member of the <a href=\"http://emberjs.com\">Ember.js</a>, <a href=\"http://rubyonrails.org\">Ruby\n            on Rails</a>\n            and <a href=\"http://www.jquery.com\">jQuery</a> Core Teams; he spends his daytime hours at the startup he\n            founded,\n            <a href=\"http://www.tilde.io\">Tilde Inc.</a>.</p>\n\n        <p>Yehuda is co-author of best-selling <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_176\">jQuery\n            in\n            Action</a> and\n            <a href=\"http://affiliate.manning.com/idevaffiliate.php?id=485_145\">Rails 3 in Action</a>.</p>\n\n        <p>He spends most of his time hacking on open source?his main projects, along with others, like <a\n                href=\"https://github.com/wycats/thor\">Thor</a>,\n            <a href=\"http://www.handlebarsjs.com\">Handlebars</a> and <a\n                    href=\"https://github.com/carlhuda/janus\">Janus</a>?or\n            traveling the world doing evangelism work.</p>\n\n        <p>He can be found on Twitter as <a href=\"http://www.twitter.com/wycats\">@wycats</a>.</p>\n    </div>\n\n</div>\n");
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["application"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options, escapeExpression=this.escapeExpression, self=this, helperMissing=helpers.helperMissing;
+
+function program1(depth0,data) {
+  
+  
+  data.buffer.push("Home");
+  }
+
+function program3(depth0,data) {
+  
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options;
+  data.buffer.push("\n                                <li class=\"list-group-item\">\n                                ");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(4, program4, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || depth0['link-to']),stack1 ? stack1.call(depth0, "item", options) : helperMissing.call(depth0, "link-to", "item", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n                                </li>\n                            ");
+  return buffer;
+  }
+function program4(depth0,data) {
+  
+  var hashTypes, hashContexts;
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "item", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  }
+
+  data.buffer.push("<div>\n    <nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\">\n        <div class=\"navbar-header\">\n            <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\".navbar-ex1-collapse\">\n                <span class=\"sr-only\">Toggle navigation</span>\n                <span class=\"icon-bar\"></span>\n                <span class=\"icon-bar\"></span>\n                <span class=\"icon-bar\"></span>\n            </button>\n            <!--<p><a class=\"navbar-brand\" href=\"#\">Your App</a></p>-->\n            <span class=\"navbar-brand\">");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || depth0['link-to']),stack1 ? stack1.call(depth0, "index", options) : helperMissing.call(depth0, "link-to", "index", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("</span>\n        </div>\n        <div class=\"collapse navbar-collapse navbar-ex1-collapse\">\n        </div>\n    </nav>\n    <div class=\"container\" id=\"main\">\n        <div class=\"row\">\n            <div>\n                <div class=\"col-md-3\">\n                    <div class=\"well sidebar-nav\">\n                        <strong>Menu</strong>\n                        <ul class=\"list-group\">\n                            ");
+  hashTypes = {};
+  hashContexts = {};
+  stack2 = helpers.each.call(depth0, "item", "in", "controller", {hash:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0,depth0,depth0],types:["ID","ID","ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n                        </ul>\n                    </div>\n                </div>\n                <div class=\"col-md-9\">\n                    ");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "outlet", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("\n                </div>\n            </div>\n        </div>\n    </div>\n</div>\n");
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["index"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options, self=this, helperMissing=helpers.helperMissing;
+
+function program1(depth0,data) {
+  
+  
+  data.buffer.push("About");
+  }
+
+function program3(depth0,data) {
+  
+  
+  data.buffer.push("r2kd805");
+  }
+
+  data.buffer.push("<div class=\"well\">\n<div>\n	");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || depth0['link-to']),stack1 ? stack1.call(depth0, "about", options) : helperMissing.call(depth0, "link-to", "about", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("<br />\n    ---<br />\n    ");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || depth0['link-to']),stack1 ? stack1.call(depth0, "r2kd805", options) : helperMissing.call(depth0, "link-to", "r2kd805", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("<br />\n</div>\n</div>\n");
+  return buffer;
+  
+});
+
+Ember.TEMPLATES["r2kd805"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options, self=this, helperMissing=helpers.helperMissing;
+
+function program1(depth0,data) {
+  
+  
+  data.buffer.push("about");
+  }
+
+  data.buffer.push("<div class='well r2kd805'>\n    <div class=\"largewidth\">\n        ");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || depth0['link-to']),stack1 ? stack1.call(depth0, "about", options) : helperMissing.call(depth0, "link-to", "about", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n    </div>\n    <div class=\"container\">\n        http://www.slideshare.net/NicoleLazzaro/creating-an-mso-viral-emotions-and-the-keys-to-social-play-gdc-china-2009\n        mechanic/choice/emotion\n    </div>\n    <div>\n\n        group: paper.li\n        [#repression](http://paper.li/tag/repression)\n        [#liquidnews](http://paper.li/tag/liquidnews)\n        [tweets from members of twitter-list rogeraaut/u](http://paper.li/rogeraaut/u)\n        [#router](http://paper.li/tag/router)\n\n        is there a paperli REST / or doc- API?\n        json - no\n        xml - no\n\n        no, no API planned, but RSS support:\n\n        Paper RSS feed\n        We've added an RSS feed option for all Pro papers. You can keep the feed private, or activate it for you readers\n        via the Add-ons tab in your dashboard. The standard RSS icon will appear at the top of your paper next to the\n        archives. [here](http://support.paper.li/entries/23951411-New-White-label-custom-CSS-collaboration-RSS)\n\n\n        ---\n        made with vi *(today)\n\n    </div>\n\n\n</div>\n");
+  return buffer;
+  
+});
